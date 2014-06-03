868525f853fec00353796054bc681b337e956d86
maxmcd <max.t.mcdonnell@gmail.com>
2014-06-03 12:29:04 -0400

    add deploy script

---
+++ site/deploy.sh
@@ -0,0 +1,8 @@
+echo -e "\033[1;34;40mRemoving old _site/ \033[0m"
+rm -rf _site/
+echo -e "\033[1;34;40mRunning 'jekyll build' \033[0m"
+jekyll build
+echo -e "\033[1;34;40mPushing to s3 \033[0m"
+s3cmd put -r _site/ s3://stephaniejonesstudio.com/site/
+echo -e "\033[1;34;40mDone \033[0m"
+# chmod 755 deploy.sh
